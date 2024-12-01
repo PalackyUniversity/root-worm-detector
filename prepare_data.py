790f10b99f9250a933204de79e51863fd509dec2
Tadeáš Fryčák <tadeas@frycak.com>
2024-12-01 20:21:44 +0100

    fix: prepare data file

--- prepare_data.py
+++ prepare_data.py
@@ -1,15 +1,53 @@
+from sahi.slicing import slice_image
 from tqdm import tqdm
-import random
-import glob
 import os
 
-ENDING = ".png"
-Y_FOLDER = "y-sliced"
-X_FOLDER = "x-sliced"
+# Parameters for slicing
+IMAGE_SIZE = 512
+IMAGE_OVERLAP = 0.2
 
-for f in tqdm(glob.glob(f"{Y_FOLDER}/*{ENDING}")):
-    folder = "train" if random.random() <= 0.7 else "valid"
-    os.makedirs(f"data/{folder}/images", exist_ok=True)
-    os.makedirs(f"data/{folder}/labels", exist_ok=True)
-    os.system(f"cp {f} data/{folder}/labels/")
-    os.system(f"cp {f.replace(Y_FOLDER, X_FOLDER)} data/{folder}/images/")
+# Define paths
+SOURCE_X_DIR = "x-original/"
+SOURCE_Y_DIR = "y-original/"
+SLICED_X_DIR = "x-sliced/"
+SLICED_Y_DIR = "y-sliced/"
+
+# Define formats
+SOURCE_X_FORMAT = ".png"
+SOURCE_Y_FORMAT = ".tif"
+
+# Create output directories if they don't exist
+os.makedirs(SLICED_X_DIR, exist_ok=True)
+os.makedirs(SLICED_Y_DIR, exist_ok=True)
+
+# Loop through images and annotations
+for annotation_filename in tqdm(os.listdir(SOURCE_Y_DIR)):
+    if annotation_filename.endswith(SOURCE_X_FORMAT):
+        # Get corresponding annotation file
+        image_filename = annotation_filename.replace(SOURCE_X_FORMAT, SOURCE_Y_FORMAT)
+
+        # Paths for source and annotation
+        image_path = os.path.join(SOURCE_X_DIR, image_filename)
+        annotation_path = os.path.join(SOURCE_Y_DIR, annotation_filename)
+
+        # Slice source image
+        slice_image(
+            image=image_path,
+            output_file_name=os.path.basename(image_path).split(".")[0],
+            output_dir=SLICED_X_DIR,
+            slice_height=IMAGE_SIZE,
+            slice_width=IMAGE_SIZE,
+            overlap_height_ratio=IMAGE_OVERLAP,
+            overlap_width_ratio=IMAGE_OVERLAP
+        )
+
+        # Slice annotation
+        slice_image(
+            image=annotation_path,
+            output_file_name=os.path.basename(annotation_path).split(".")[0],
+            output_dir=SLICED_Y_DIR,
+            slice_height=IMAGE_SIZE,
+            slice_width=IMAGE_SIZE,
+            overlap_height_ratio=IMAGE_OVERLAP,
+            overlap_width_ratio=IMAGE_OVERLAP
+        )
